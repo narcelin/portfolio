526a82af949ffa68b01fd311a726cf87aaa0848f
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-03-11 19:02:30 +0100

    Chapter 4

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* Adding font throughout the application w/ tailwind antialised */}
+      <body className={`${inter.className} antialised`}>{children}</body> 
     </html>
   );
 }
